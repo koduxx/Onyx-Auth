76ca0c8cd82e2d9ce144ae6f89459f802fee9e62
koduxx <79064088+helmetboy1@users.noreply.github.com>
2021-05-03 21:12:35 +0100

    Initial commit

---
+++ Exchange.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace onyx.LOGIN
+{
+  public class Exchange
+  {
+    [JsonPropertyName("expiresInSeconds")]
+    public int ExpiresInSeconds { get; set; }
+
+    [JsonPropertyName("code")]
+    public string Code { get; set; }
+
+    [JsonPropertyName("creatingClientId")]
+    public string CreatingClientId { get; set; }
+  }
+}
